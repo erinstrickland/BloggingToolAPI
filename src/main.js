4ce06c2bfd51e4ae826e078f519ad6b97a43da8d
Erin Strickland <stricklande93@gmail.com>
2018-10-14 18:47:16 -0400

    Added picture download

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const koaBody = require('koa-body')
 const rp = require('request-promise-native')
+var request = require('request')
+const fs = require('fs')
 
 const app = new Koa()
 const router = new Router()
@@ -11,8 +13,17 @@ router.get('/imagesearch', async (ctx, next) => {
     const searchTerm = ctx.request.query.keyword
     const htmlString = await rp(`http://www.pixabay.com/api/?key=${pbKey}&q=${searchTerm}&image_type=photo`).catch((err) => { console.log(err) })
     const imageList = JSON.parse(htmlString)
-    const display = imageList.hits.map(image => image.webformatURL)
+    const display = imageList.hits.map(image => {
+        return {url: image.previewURL, fullSizeURL: image.fullHDURL}
+    })
     ctx.body = display
+})
+
+router.put('/download', async (ctx, next) => {
+    const searchId = ctx.request.query.imageURL
+      request(searchId, {encoding: 'binary'}, (error, response, body) => {
+        fs.writeFile('download.jpg', body, 'binary', (err) => {})
+      })
 })
 
 app.use(koaBody())
